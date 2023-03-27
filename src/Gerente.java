207eb2cf356f34705a3e947d42494ed10a7c639b
paulo <henriquepaulo0606@gmail.com>
2023-03-27 16:52:04 -0300

    Criando classe funcionario e trabalhando com herança

---
+++ src/Gerente.java
@@ -0,0 +1,21 @@
+// gerente é um funcionario, Gerente herda do funcionario
+public class Gerente extends Funcionario {
+
+    private int senha = 0;
+
+    public int getSenha() {
+        return senha;
+    }
+
+    public void setSenha(int senha) {
+        this.senha = senha;
+    }
+    // Criando metodo autentica que recebe senha como parametro obs esse metodo é especifica da classe Gerente
+    public boolean autentica(int senha){
+        if (this.senha == senha ) {
+            return true;
+        } else {
+            return  false;
+        }
+    }
+}
---
+++ src/TesteGerente.java
@@ -0,0 +1,19 @@
+public class TesteGerente {
+    public static void main(String[] args) {
+        // Instanciando classe gerente e verificado se ele herdou da classe funcionario
+        Gerente g1 = new Gerente();
+        g1.setNome("Marcos");
+        g1.setCpf("223355646-9");
+        g1.setSalario(2600.00);
+
+        System.out.println(g1.getNome());
+        System.out.println(g1.getCpf());
+        System.out.println(g1.getSalario());
+        // Criando senha pro metodo autentica
+        g1.setSenha(1234);
+        // Criando variavel pra verificar se autentica a senha do gerente marcos
+        boolean autenticou = g1.autentica(1234); // esse metodo esta dentro da classe gerente
+        // Printando na tela pra ver se autenticou
+        System.out.println(autenticou);
+    }
+}
